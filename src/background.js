c30b85089ade624c2af8e24f3da79add13b66373
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-06-22 23:08:23 -0400

    before cleanup

--- src/background.js
+++ src/background.js
@@ -1,15 +1,29 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ color: '#3aa757' }, function () {
+        console.log("The color is green.");
     });
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'www.surveymonkey.com' },
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
-      });
-  });
+    });
+});
+
+// from: https://stackoverflow.com/a/19758800/105999
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    alert('foo here');
+    // ...check the URL of the active tab against our pattern and...
+    //if (urlRegex.test(tab.url)) {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+    //}
+});
