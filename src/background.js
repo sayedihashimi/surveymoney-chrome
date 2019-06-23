62fca5acbb6060693bd3d3d5df6697ce0e95f9d5
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-06-23 00:23:16 -0400

    adding button to page. code needs cleanup

--- src/background.js
+++ src/background.js
@@ -13,17 +13,3 @@ chrome.runtime.onInstalled.addListener(function () {
         }]);
     });
 });
-
-// from: https://stackoverflow.com/a/19758800/105999
-function doStuffWithDom(domContent) {
-    console.log('I received the following DOM content:\n' + domContent);
-}
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-    alert('foo here');
-    // ...check the URL of the active tab against our pattern and...
-    //if (urlRegex.test(tab.url)) {
-        // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
-    //}
-});
---
+++ src/content.js
@@ -0,0 +1,35 @@
+
+console.log("content script2 loaded");
+
+function addButton(){
+    var newTagElements = $("a:not([ce-processed])[ta-new-category-btn]");
+    // first add the attribute to prevent duplication
+    newTagElements.attr('ce-processed','');
+
+    $("<a class='wds-button wds-button--ghost wds-button--sm' href='#'>↑ Export tags</a>")
+        .insertAfter( newTagElements );
+}
+
+function registerDomWatcherToInsertElement() {
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    var observer = new MutationObserver(function (mutations, observer) {
+        // fired when a mutation occurs
+        // console.log(mutations, observer);
+        console.log('calling addButton')
+        addButton();
+    });
+    // TODO: can this be fine tuned?
+    observer.observe(document, {
+        subtree: true,
+        childList:true
+    });
+}
+
+async function extensionMain() {
+    registerDomWatcherToInsertElement();
+    addButton();
+    
+}
+
+extensionMain();
